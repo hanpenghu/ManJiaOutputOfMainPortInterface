c49a1a3e8d978e36426bdab1ad3235ecffcbbf77
hanpenghu <563909408@qq.com>
2017-06-09 15:12:22 +0800

    2017_6_9   weekday(5)   14:57:49

--- src/com/ManJia/MainPortInterface/component/ValidateInterceptor.java
+++ src/com/ManJia/MainPortInterface/component/ValidateInterceptor.java
@@ -6,7 +6,7 @@ import com.ManJia.MainPortInterface.utils.ReadTxt;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.LogManager;//
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
