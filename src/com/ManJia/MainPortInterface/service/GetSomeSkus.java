2ca2921c1d85b2fd6830c2e086e9a7ba8eca7bce
hanpenghu <563909408@qq.com>
2017-06-14 13:25:12 +0800

    2017_6_14   weekday(3)   13:25:08

---
+++ src/com/ManJia/MainPortInterface/service/GetSomeSkus.java
@@ -0,0 +1,15 @@
+package com.ManJia.MainPortInterface.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+@Component
+public class GetSomeSkus {
+
+//    @Autowired
+
+
+}
